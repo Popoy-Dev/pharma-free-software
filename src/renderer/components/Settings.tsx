2281f832a3696a942628b93cd648670f154a3bcd
Popoy-Dev <popoykua28@gmail.com>
2023-10-27 13:01:09 +0800

    settings form

--- src/renderer/components/Settings.tsx
+++ src/renderer/components/Settings.tsx
@@ -1,10 +1,11 @@
-import { Breadcrumb, Button, Form, Input, InputNumber } from 'antd';
-import React, { useState } from 'react';
+import { Breadcrumb, Button, Form, Input, Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
 import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 
 function Settings() {
   const [isSetting, setIsSetting] = useState(true);
   const [isActivation, setIsActivation] = useState(false);
+  const [pharmacyInfo, setPharmacyInfo] = useState<any>('');
 
   type FieldType = {
     cashier_name?: string;
@@ -13,7 +14,7 @@ function Settings() {
     tin_no?: string;
     cell_no?: string;
   };
-
+  const [form] = Form.useForm();
   const onFinish = (values: any) => {
     let ownerId: any = Math.floor(Math.random() * 10000);
     // eslint-disable-next-line no-unused-expressions
@@ -22,15 +23,27 @@ function Settings() {
       ...values,
       ownerId,
     };
-    console.log('Success:', combinedValue, values.pharmacy_name);
+
+    localStorage.setItem('user', JSON.stringify(combinedValue));
   };
+
+  useEffect(() => {
+    const value: any = localStorage.getItem('user');
+    const parseData = JSON.parse(value);
+    setPharmacyInfo(parseData);
+
+    if (parseData) {
+      // Set initial form values and disable the form fields
+      form.setFieldsValue({ ...parseData });
+      // setIsFormDisabled(true);
+    }
+  }, []);
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
 
   const handleSetting = () => {
-    console.log('hello');
     setIsSetting(true);
     setIsActivation(false);
   };
@@ -64,62 +77,69 @@ function Settings() {
     // eslint-disable-next-line no-unused-expressions
     isSetting ? (
       <div>
-        <div style={{ width: '50%', margin: 'auto', marginTop: '36px' }}>
-          <h3 style={{ textAlign: 'left' }}>Fill up for receipt details</h3>
+        {!pharmacyInfo ? (
+          <Spin />
+        ) : (
+          <div style={{ width: '50%', margin: 'auto', marginTop: '36px' }}>
+            <h3 style={{ textAlign: 'left' }}>Fill up for receipt details</h3>
+            <Form
+              name="basic"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600 }}
+              onFinish={onFinish}
+              initialValues={pharmacyInfo}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item<FieldType>
+                label="Pharmacy Name"
+                name="pharmacy_name"
+                rules={[{ required: true, message: 'Please input pharmacy name!' }]}
+              >
+                <Input />
+              </Form.Item>
 
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item<FieldType>
-              label="Pharmacy Name"
-              name="pharmacy_name"
-              rules={[{ required: true, message: 'Please input pharmacy name!' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item<FieldType>
-              label="Address"
-              name="address"
-              rules={[{ required: true, message: 'Please input shop address' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item<FieldType>
-              label="TIN number"
-              name="tin_no"
-              rules={[{ required: true, message: 'Please input TIN number' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item<FieldType>
-              label="Phone number"
-              name="cell_no"
-              rules={[{ required: true, message: 'Please input phone number!' }]}
-            >
-              <InputNumber style={{ width: '100%' }} maxLength={11} />
-            </Form.Item>
-            <Form.Item<FieldType>
-              label="Cashier Name"
-              name="cashier_name"
-              rules={[{ required: true, message: 'Please input cashier name!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
+              <Form.Item<FieldType>
+                label="Address"
+                name="address"
+                rules={[{ required: true, message: 'Please input shop address' }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item<FieldType>
+                label="TIN number"
+                name="tin_no"
+                rules={[{ required: true, message: 'Please input TIN number' }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item<FieldType>
+                label="Phone number"
+                name="cell_no"
+                rules={[{ required: true, message: 'Please input phone number!' }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item<FieldType>
+                label="Cashier Name"
+                name="cashier_name"
+                rules={[{ required: true, message: 'Please input cashier name!' }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ display: pharmacyInfo ? '' : '' }}
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        )}
       </div>
     ) : (
       ''
